a5e816d910dcffc44e6ddb293a07b0272d48192c
viniciusfariac <vinicius.cerqueira@sptech.school>
2026-05-04 20:52:12 -0300

    feat: adicionando checagem do progresso do circulo, botão de seleção de dia, botão de adicionar mais um exercício e remover

--- data_base/query_analise.sql
+++ data_base/query_analise.sql
@@ -54,3 +54,146 @@ AND
 AND 
 	e.exercise_name = 'Supino inclinado com halteres'
 ORDER BY tr.training_date, ts.set_order;
+
+-- Frequencia geral
+SELECT 
+    DISTINCT DATE(tr.training_date) dia,
+    DAYNAME(tr.training_date) dia_semana
+FROM
+	user u
+JOIN training t ON u.id_user = t.user_id
+JOIN training_exercise te ON te.training_id = t.id_training
+JOIN exercise e ON e.id_exercise = te.exercise_id
+JOIN training_log tr ON tr.training_exercise_id = te.id_training_exercise
+JOIN training_set ts ON ts.training_log_id = tr.id_training_log
+WHERE
+	u.username = 'Vinicius';
+    
+-- Frequência no mês
+SELECT 
+	MONTH(tr.training_date) AS mes,
+    YEAR(tr.training_date) AS ano,
+    COUNT(DISTINCT tr.training_date) AS frequencia
+FROM training_log tr
+JOIN training_exercise te ON te.id_training_exercise = tr.training_exercise_id
+JOIN training t ON t.id_training = te.training_id
+JOIN user u ON u.id_user = t.user_id
+WHERE u.username = 'vinicius'
+GROUP BY mes, ano
+ORDER BY mes, ano;
+
+-- Frequência por semana
+SELECT 
+	DAYNAME(tr.training_date) dia_semana,
+    COUNT(DISTINCT DATE(tr.training_date)) AS frequencia
+FROM training_log tr
+JOIN training_exercise te ON te.id_training_exercise = tr.training_exercise_id
+JOIN training t ON t.id_training = te.training_id
+JOIN user u ON u.id_user = t.user_id
+WHERE u.username = 'vinicius'
+GROUP BY dia_semana
+ORDER BY frequencia DESC;
+
+-- Frequência por semana no mês
+SELECT 
+	DAYNAME(tr.training_date) dia_semana,
+    COUNT(DISTINCT DATE(tr.training_date)) AS frequencia
+FROM training_log tr
+JOIN training_exercise te ON te.id_training_exercise = tr.training_exercise_id
+JOIN training t ON t.id_training = te.training_id
+JOIN user u ON u.id_user = t.user_id
+WHERE 
+	u.username = 'vinicius'
+AND
+	MONTH(tr.training_date) = 4
+GROUP BY dia_semana
+ORDER BY frequencia DESC;
+
+-- Maior carga em determinado exercício
+SELECT 
+    t.name_training,
+    e.exercise_name,
+    MAX(ts.weight) pr
+FROM
+	user u
+JOIN training t ON u.id_user = t.user_id
+JOIN training_exercise te ON te.training_id = t.id_training
+JOIN exercise e ON e.id_exercise = te.exercise_id
+JOIN training_log tr ON tr.training_exercise_id = te.id_training_exercise
+JOIN training_set ts ON ts.training_log_id = tr.id_training_log
+WHERE
+	u.username = 'Vinicius'
+AND
+	t.name_training = 'Upper A'
+AND 
+	e.exercise_name = 'Supino inclinado com halteres'
+ORDER BY tr.training_date, ts.set_order;
+
+-- Quantas vezes fez cada treino
+SELECT
+    DISTINCT t.name_training, 
+    COUNT(DISTINCT DATE(tl.training_date)) quantas_vezes
+FROM
+	training t
+JOIN user u ON u.id_user = t.user_id
+JOIN training_exercise te ON te.training_id = t.id_training
+JOIN training_log tl ON tl.training_exercise_id = te.id_training_exercise
+GROUP BY t.name_training;
+
+-- Média de reps por exercício
+SELECT 
+    t.name_training,
+    e.exercise_name,
+    TRUNCATE(AVG(ts.rep), 2) media_rep
+FROM
+	user u
+JOIN training t ON u.id_user = t.user_id
+JOIN training_exercise te ON te.training_id = t.id_training
+JOIN exercise e ON e.id_exercise = te.exercise_id
+JOIN training_log tr ON tr.training_exercise_id = te.id_training_exercise
+JOIN training_set ts ON ts.training_log_id = tr.id_training_log
+WHERE
+	u.username = 'Vinicius'
+AND
+	t.name_training = 'Upper A'
+AND 
+	e.exercise_name = 'Supino inclinado com halteres'
+ORDER BY tr.training_date, ts.set_order;
+
+-- Evolução de carga
+SELECT 
+    DISTINCT (tr.training_date),
+    MAX(ts.weight)
+FROM
+	user u
+JOIN training t ON u.id_user = t.user_id
+JOIN training_exercise te ON te.training_id = t.id_training
+JOIN exercise e ON e.id_exercise = te.exercise_id
+JOIN training_log tr ON tr.training_exercise_id = te.id_training_exercise
+JOIN training_set ts ON ts.training_log_id = tr.id_training_log
+WHERE
+	u.username = 'Vinicius'
+AND
+	t.name_training = 'Upper A'
+AND 
+	e.exercise_name = 'Supino inclinado com halteres'
+GROUP BY tr.training_date;
+
+-- Média de rep por treino
+SELECT 
+	DATE(tr.training_date),
+    TRUNCATE(AVG(ts.rep), 2)
+FROM
+	user u
+JOIN training t ON u.id_user = t.user_id
+JOIN training_exercise te ON te.training_id = t.id_training
+JOIN exercise e ON e.id_exercise = te.exercise_id
+JOIN training_log tr ON tr.training_exercise_id = te.id_training_exercise
+JOIN training_set ts ON ts.training_log_id = tr.id_training_log
+WHERE
+	u.username = 'Vinicius'
+AND
+	t.name_training = 'Upper A'
+AND 
+	e.exercise_name = 'Supino inclinado com halteres'
+GROUP BY DATE(tr.training_date);
--- public/js/animation.js
+++ public/js/animation.js
@@ -12,3 +12,127 @@ function showPass(ipt_pass, ipt_confirm) {
         confirm.classList.replace('ph-eye-slash', 'ph-eye')
     }
 }
+
+function toggleSelectButton(type) {
+    let day = document.getElementById(type)
+
+    if (day.classList.contains("selected")) {
+        day.classList.remove("selected")
+        let index = listDay.indexOf(type)
+        listDay.splice(index, 1)
+    } else {
+        day.classList.add("selected")
+        listDay.push(type)
+
+    }
+    checkSecondCircle()
+}
+
+function checkFirstCircle() {
+    let ipt_name = input_name.value
+    let ipt_type = input_type.value
+    let step = document.getElementById('step1')
+    let circle = document.getElementById('circle1')
+
+    if (ipt_name && ipt_type) {
+        step.classList.remove("active")
+        step.classList.add("completed")
+        circle.innerHTML = "✔"
+    } else {
+        step.classList.remove("completed")
+        step.classList.add("active")
+        circle.innerHTML = "1"
+    }
+}
+
+function checkSecondCircle() {
+    let day_len = listDay.length
+    let step = document.getElementById('step2')
+    let circle = document.getElementById('circle2')
+
+    if (day_len > 0) {
+        step.classList.remove("active")
+        step.classList.add("completed")
+        circle.innerHTML = "✔"
+    } else {
+        step.classList.remove("completed")
+        step.classList.add("active")
+        circle.innerHTML = "1"
+    }
+}
+
+function addOneMore() {
+    let box = document.getElementById("box_exercise")
+    let cards = document.querySelectorAll("#card_exercise")
+    let len = cards.length
+    box.insertAdjacentHTML('beforeend', `
+                       <div class="card_exercise" id="card_exercise">
+                        <div class="title_exercise">
+                            <p id="title_exercise">Exercício ${len + 1}</p>
+                        </div>
+                        <div class="content_exercise">
+                            <div class="container_choices">
+                                <div class="choice_group">
+                                    <span>Grupo muscular</span>
+                                    <select id="">
+                                        <option value="">Peito</option>
+                                        <option value="">Ombro</option>
+                                        <option value="">Triceps</option>
+                                        <option value="">Quadriceps</option>
+                                        <option value="">Glúteos</option>
+                                        <option value="">Costas</option>
+                                        <option value="">Bíceps</option>
+                                        <option value="">Antebraço</option>
+                                        <option value="">Panturrilha</option>
+                                        <option value="">Adbomên</option>
+                                    </select>
+                                </div>
+                                <div class="choice_group">
+                                    <span>Exercício</span>
+                                    <select id="">
+                                        <option value="">Supino</option>
+                                        <option value="">Pulley</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="choice_set">
+                                <p>Séries para esse exercício</p>
+                                <input type="number" id="ipt_exercise${len + 1}" oninput="checkThirdCircle()">
+                            </div>
+                        </div>
+                    </div>
+    `)
+    checkThirdCircle()
+}
+
+function removeOneMore() {
+    let box = document.getElementById("box_exercise")
+    let cards = document.querySelectorAll("#card_exercise")
+    let card = cards[cards.length - 1]
+    card.remove()
+    checkThirdCircle()
+}
+
+function checkThirdCircle() {
+    let cards = document.querySelectorAll("#card_exercise")
+    let step = document.getElementById('step3')
+    let circle = document.getElementById('circle3')
+    let count = 0
+    for (let i = 1; i <= cards.length; i++) {
+        const input = document.getElementById(`ipt_exercise${i}`)
+
+        if (input.value) {
+            count++
+        }
+    }
+
+    if (count == cards.length) {
+        step.classList.remove("active")
+        step.classList.add("completed")
+        circle.innerHTML = "✔"
+    } else {
+        step.classList.remove("completed")
+        step.classList.add("active")
+        circle.innerHTML = "3"
+    }
+}
